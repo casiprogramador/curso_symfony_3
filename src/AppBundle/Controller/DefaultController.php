9062df9847b3140cf3942d0e7c6f5d74f1c47d36
Marcelo Choque <casiprogramador@gmail.com>
2016-12-20 01:21:48 -0400

    9. Creacion de primera vista

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,10 +12,7 @@ class DefaultController extends Controller
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+    {	
+		return $this->render('layout/base.html.twig');
     }
 }
